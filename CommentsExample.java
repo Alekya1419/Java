db009f52f21b2b34df89c025d4aa1394a2634deb
Alekya1419 <alekya19dec@gmail.com>
2025-03-06 14:24:52 +0530

    Add files via upload

---
+++ CommentsExample.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+public class CommentsExample {
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /* This is a 
+           multi-line comment */
+
+        /**
+         * This is a documentation comment
+         * It is used to describe methods, classes, etc.
+         */
+        System.out.println("Comment examples in Java");
+    }
+}
